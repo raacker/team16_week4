dbdd6b06eeb60fb7329b0cedf551afc69a02ca81
raacker <haven.cpp@gmail.com>
2016-03-31 17:49:53 +0900

    init project. packages, files

---
+++ README.md
@@ -0,0 +1,5 @@
+#Team16_week4
+
+This is repository of team 16.
+
+Mainly, it covers simple bill system of week 3
---
+++ src/main/java/org/team16/team16_week4/App.java
@@ -0,0 +1,13 @@
+package org.team16.team16_week4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
