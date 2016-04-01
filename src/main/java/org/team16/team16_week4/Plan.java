cc47b3b7cc46193a4c8bb4a6f3a3c6a5e73ca6bb
MinSungKing <vnfflrhs@gmail.com>
2016-04-01 14:16:14 +0900

    make Plan

--- src/main/java/org/team16/team16_week4/Plan.java
+++ src/main/java/org/team16/team16_week4/Plan.java
@@ -1,5 +1,14 @@
 package org.team16.team16_week4;
 
 public interface Plan {
-
+	public String getPlanName();
+	
+	public double getBasicMonthlyRate();
+	
+	public int getIncludeMinutes();
+	
+	public double getRatePerExcessMinute();
+	
+	public double getAdditionalLineRate();
+	
 }
