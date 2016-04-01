8c6c4b0afcc902f62d978938802ab131fc4ad555
raacker <haven.cpp@gmail.com>
2016-04-01 14:00:45 +0900

    Modified User.java

--- src/main/java/org/team16/team16_week4/User.java
+++ src/main/java/org/team16/team16_week4/User.java
@@ -1,5 +1,38 @@
 package org.team16.team16_week4;
 
 public class User {
-
+	private Plan _plan;
+	private int _numberOfLines;
+	private int _usedMinutes;
+	private boolean _familyDiscount;
+	
+	public User(Plan plan, int lines, int minutes)
+	{
+		this._plan = plan;
+		this._numberOfLines = lines;
+		this._usedMinutes = minutes;
+		if(this._numberOfLines >= 3)
+			this._familyDiscount = true;
+		else
+			this._familyDiscount = false;
+	}
+	
+	public Plan getUserPlan()
+	{
+		return this._plan;
+	}
+	public int getNumberOfLines()
+	{
+		return this._numberOfLines;
+	}
+	public int getUsedMinutes()
+	{
+		return this._usedMinutes;
+	}
+	public boolean getFamilyDiscount()
+	{
+		return this._familyDiscount;
+	}
+	
+	
 }
