94293d2eeb3de087692cb4fc37a147613000b31a
jhlee1108 <jeonghyeon.lee1108@gmail.com>
2016-04-01 14:15:41 +0900

    Gold, Sliver 추가

--- src/main/java/org/team16/team16_week4/Gold.java
+++ src/main/java/org/team16/team16_week4/Gold.java
@@ -1,5 +1,37 @@
 package org.team16.team16_week4;
 
-public class Gold {
+public class Gold implements Plan {
+	private String planName;
+	private double basicMonthlyRate;
+	private int includeMinutes;
+	private double ratePerExcessMinute;
+	private double additionalLineRate;
 
+	public Gold(){
+		this.planName = "Gold";
+		this.basicMonthlyRate = 49.95;
+		this.includeMinutes = 1000;
+		this.ratePerExcessMinute = 0.45;
+		this.additionalLineRate = 14.50;
+	}
+
+	public String getPlanName() {
+		return this.planName;
+	}
+
+	public double getBasicMonthlyRate() {
+		return this.basicMonthlyRate;
+	}
+
+	public int getIncludeMinutes() {
+		return this.includeMinutes;
+	}
+
+	public double getRatePerExcessMinute() {
+		return this.ratePerExcessMinute;
+	}
+
+	public double getAdditionalLineRate() {
+		return this.additionalLineRate;
+	}
 }
--- src/main/java/org/team16/team16_week4/Silver.java
+++ src/main/java/org/team16/team16_week4/Silver.java
@@ -1,5 +1,37 @@
 package org.team16.team16_week4;
 
-public class Silver {
+public class Silver implements Plan {
+	private String planName;
+	private double basicMonthlyRate;
+	private int includeMinutes;
+	private double ratePerExcessMinute;
+	private double additionalLineRate;
 
+	public Silver(){
+		this.planName = "Silver";
+		this.basicMonthlyRate = 29.95;
+		this.includeMinutes = 500;
+		this.ratePerExcessMinute = 0.54;
+		this.additionalLineRate = 21.50;
+	}
+
+	public String getPlanName() {
+		return this.planName;
+	}
+
+	public double getBasicMonthlyRate() {
+		return this.basicMonthlyRate;
+	}
+
+	public int getIncludeMinutes() {
+		return this.includeMinutes;
+	}
+
+	public double getRatePerExcessMinute() {
+		return this.ratePerExcessMinute;
+	}
+
+	public double getAdditionalLineRate() {
+		return this.additionalLineRate;
+	}
 }
